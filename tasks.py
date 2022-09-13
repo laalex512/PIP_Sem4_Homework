87e5994a10605f4290220dac5c833bbffd2c715b
laalex <laalex512@gmail.com>
2022-09-13 22:16:36 +0300

    Add Task 33

--- tasks.py
+++ tasks.py
@@ -42,3 +42,57 @@
 
 # resultSet = set(initList)
 # print(resultSet)
+
+
+# Task 33
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100)
+# многочлена и записать в файл многочлен степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+# import random
+
+# k = 26
+# coefficientList = []
+# for i in range(k+1):
+#     coefficientList.append(random.randint(0, 100))
+# print(coefficientList)
+
+
+# indexes = {                                     # словарь степеней в unicode
+#     0: "\u2070",
+#     1: "\u00B9",
+#     2: "\u00B2",
+#     3: "\u00B3",
+#     4: "\u2074",
+#     5: "\u2075",
+#     6: "\u2076",
+#     7: "\u2077",
+#     8: "\u2078",
+#     9: "\u2079",
+# }
+
+# # строка для терминала с красивыми степенями
+# resultString = ""
+# for i in range(k-1):
+#     kString = str(k-i)
+#     currentPower = ""                           # для создания двузначной степени
+#     for j in range(len(kString)):
+#         currentPower += indexes[int(kString[j])]
+#     resultString += f"{str(coefficientList[i])}x{currentPower} + "
+# resultString += f"{str(coefficientList[k-1])}x + "
+# resultString += f"{str(coefficientList[k])} = 0"
+
+
+# # строка для файла (не хочет записывать с красивыми степенями))
+# resultStringForFile = ""
+# for i in range(k-1):
+#     resultStringForFile += f"{coefficientList[i]}x**{k-i} + "
+# resultStringForFile += f"{str(coefficientList[k-1])}x + "
+# resultStringForFile += f"{str(coefficientList[k])} = 0"
+
+
+# print(resultString)
+# f = open("task33.txt", "w")
+# f.write(resultStringForFile)
